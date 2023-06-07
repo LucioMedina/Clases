7dbec531c6562fba66bcd1ed56b53748f852d8c1
richard <1303455@senati.pe>
2023-06-07 13:27:36 -0500

    Avanze de entregable

---
+++ Entregable ejercicios en R.R
@@ -0,0 +1,22 @@
+#Ejercicio 1
+listRand <- c(sample(1:100, size = 30, replace = FALSE))
+
+#Ejercicio 2
+personas <- c("Lucio", "Angel", "Mirian", "Israel", 
+              "Sofia", "Pedro", "Juan", "Jose",
+              "Manuel", "Francisco", "Leonardo", "Diego")
+
+#Ejercicio 3
+paises <- c("Perú", "Argentina", "Colombia", "Chile",
+            "Ecuador", "Venezuela", "Cuba", "Puerto Rico",
+            "República Dominicana", "Francia", "Italia",
+            "España", "Croacia", "Canadá", "Marruecos")
+
+#Ejercicio 4
+iPersonas <- c(rev(personas))
+
+#Ejercicio 5
+oPaises <- c(rev(paises))
+
+#Ejercicio 6
+matrixNum <- matrix(1:120)
